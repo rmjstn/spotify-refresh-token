9cac11a82a6a706320f6b3fd64e36d3159ce0953
Rémy Jestin <remy.jestin@gmail.com>
2026-02-23 19:43:46 +0100

    Spotify now requires HTTPS

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = '463fc0969d9240fe8d9a987478380b76'; // Your client id
 var client_secret = 'aa0d8021beeb4fda9066b177d902c698'; // Your client secret
-var redirect_uri = 'http://localhost:8888/callback';
+var redirect_uri = 'https://localhost:8888/callback';
 
 var app = express();
 
